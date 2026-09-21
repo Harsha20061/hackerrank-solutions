1873ea28af33a43f516b453c3a73887f0ece451b
varla chandra harsha <chandraharshavarala@gmail.com>
2026-09-21 22:04:03 +0530

    ✅ for loop4 [java15] — approach1

--- solutions/java15/medium/for-loop4/solution-approach1.java
+++ solutions/java15/medium/for-loop4/solution-approach1.java
@@ -6,7 +6,7 @@
 // Platform    HackerRank
 // Language    java15
 // Status      Accepted
-// Submitted   2026-09-21, 10:03 p.m.
+// Submitted   2026-09-21, 10:04 p.m.
 // ──────────────────────────────────────────────────
 
 import java.io.*;
