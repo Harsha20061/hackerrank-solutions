0f9f417ddefa64f5b0d9a7c6d4321bba22129293
varla chandra harsha <chandraharshavarala@gmail.com>
2026-09-23 12:23:18 +0530

    ✅ pattern8 1 [java15] — approach1

---
+++ solutions/java15/medium/pattern8-1/solution-approach1.java
@@ -0,0 +1,35 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/pattern-test1/challenges/pattern8-1/problem?isFullScreen=true
+// Problem     pattern8 1
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java15
+// Status      Accepted
+// Submitted   2026-09-23, 12:23 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<0){
+            System.out.println("No output generated");
+return;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int k =n;k>=i;k--){
+                 System.out.print("*");
+                
+            }
+        System.out.println();
+        }
+    }
+}
