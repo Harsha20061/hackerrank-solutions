6efecbe0c9a24a0bdc732c53ab88a0b589af46e1
varla chandra harsha <chandraharshavarala@gmail.com>
2026-09-23 22:15:47 +0530

    ✅ pattern17 2 [java15] — approach1

---
+++ solutions/java15/medium/pattern17-2/solution-approach1.java
@@ -0,0 +1,36 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/pattern-test1/challenges/pattern17-2/problem?isFullScreen=true
+// Problem     pattern17 2
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java15
+// Status      Accepted
+// Submitted   2026-09-23, 10:15 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<0){
+            System.out.println("No output generated");
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i+j==n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
